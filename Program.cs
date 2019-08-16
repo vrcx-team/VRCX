35e7de03e4b0f5d9dabf2e1c636175c5f1be8c0d
pypy <sex@example.com>
2019-08-17 04:19:01 +0900

    v2019.08.17

--- Program.cs
+++ Program.cs
@@ -15,52 +15,60 @@ namespace VRCX
         [STAThread]
         public static void Main()
         {
-            var settings = new CefSettings
+            try
             {
-                IgnoreCertificateErrors = true,
-                CachePath = "cache",
-                PersistUserPreferences = true,
-                PersistSessionCookies = true,
-                WindowlessRenderingEnabled = true
-            };
-            settings.CefCommandLineArgs.Add("disable-web-security", "1");
-            settings.CefCommandLineArgs.Add("no-proxy-server", "1");
-            settings.CefCommandLineArgs.Add("disable-plugins-discovery", "1");
-            settings.CefCommandLineArgs.Add("disable-extensions", "1");
-            settings.CefCommandLineArgs.Add("disable-pdf-extension", "1");
-            // settings.CefCommandLineArgs.Add("disable-gpu", "1");
-            settings.CefCommandLineArgs.Add("disable-direct-write", "1");
-            settings.LogSeverity = LogSeverity.Disable;
-            settings.DisableGpuAcceleration();
-            /*settings.RegisterScheme(new CefCustomScheme
+                var settings = new CefSettings
+                {
+                    IgnoreCertificateErrors = true,
+                    CachePath = "cache",
+                    PersistUserPreferences = true,
+                    PersistSessionCookies = true,
+                    WindowlessRenderingEnabled = true
+                };
+                settings.CefCommandLineArgs.Add("disable-web-security", "1");
+                settings.CefCommandLineArgs.Add("no-proxy-server", "1");
+                settings.CefCommandLineArgs.Add("disable-plugins-discovery", "1");
+                settings.CefCommandLineArgs.Add("disable-extensions", "1");
+                settings.CefCommandLineArgs.Add("disable-pdf-extension", "1");
+                // settings.CefCommandLineArgs.Add("disable-gpu", "1");
+                settings.CefCommandLineArgs.Add("disable-direct-write", "1");
+                settings.LogSeverity = LogSeverity.Disable;
+                settings.DisableGpuAcceleration();
+                /*settings.RegisterScheme(new CefCustomScheme
+                {
+                    SchemeName = "vrcx",
+                    DomainName = "app",
+                    SchemeHandlerFactory = new FolderSchemeHandlerFactory(Application.StartupPath + "/../../../html")
+                });*/
+
+                // MUST TURN ON (Error when creating a browser on certain systems.)
+                CefSharpSettings.WcfEnabled = true;
+
+                CefSharpSettings.ShutdownOnExit = false;
+                CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+
+                // Cef.EnableHighDPISupport();
+
+                if (Cef.Initialize(settings, true, browserProcessHandler: null))
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    CpuMonitor.Start();
+                    VRCXStorage.Load();
+                    VRCXVR.Setup();
+                    VRCX_LogWatcher.Start();
+                    Application.Run(new MainForm());
+                    VRCX_LogWatcher.Stop();
+                    VRCXVR.Stop();
+                    VRCXStorage.Save();
+                    CpuMonitor.Stop();
+                    Cef.Shutdown();
+                }
+            }
+            catch (Exception ex)
             {
-                SchemeName = "vrcx",
-                DomainName = "app",
-                SchemeHandlerFactory = new FolderSchemeHandlerFactory(Application.StartupPath + "/../../../html")
-            });*/
-
-            // MUST TURN ON (Error when creating a browser on certain systems.)
-            CefSharpSettings.WcfEnabled = true;
-
-            CefSharpSettings.ShutdownOnExit = false;
-            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
-
-            // Cef.EnableHighDPISupport();
-
-            if (Cef.Initialize(settings, true, browserProcessHandler: null))
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                CpuMonitor.Start();
-                VRCXStorage.Load();
-                VRCXVR.Setup();
-                VRCX_LogWatcher.Start();
-                Application.Run(new MainForm());
-                VRCX_LogWatcher.Stop();
-                VRCXVR.Stop();
-                VRCXStorage.Save();
-                CpuMonitor.Stop();
-                Cef.Shutdown();
+                MessageBox.Show($"{ex.Message}\n{ex.StackTrace}", "PLEASE REPORT TO PYPY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
             }
         }
     }
