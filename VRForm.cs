c6e8195e01bf30e68b08b849ef20b3dbb30dc094
pypy <git@pypy.moe>
2020-11-01 19:47:06 +0900

    bugfix

--- VRForm.cs
+++ VRForm.cs
@@ -46,12 +46,12 @@ namespace VRCX
             {
                 CamelCaseJavascriptNames = false
             };
-            Browser1.JavascriptObjectRepository.Register("VRCX", new VRCX(), true, options);
-            Browser1.JavascriptObjectRepository.Register("VRCXStorage", new VRCXStorage(), false, options);
-            Browser1.JavascriptObjectRepository.Register("SQLite", new SQLite(), true, options);
-            Browser2.JavascriptObjectRepository.Register("VRCX", new VRCX(), true, options);
-            Browser2.JavascriptObjectRepository.Register("VRCXStorage", new VRCXStorage(), false, options);
-            Browser2.JavascriptObjectRepository.Register("SQLite", new SQLite(), true, options);
+            Browser1.JavascriptObjectRepository.Register("VRCX", VRCX.Instance, true, options);
+            Browser1.JavascriptObjectRepository.Register("VRCXStorage", VRCXStorage.Instance, false, options);
+            Browser1.JavascriptObjectRepository.Register("SQLite", SQLite.Instance, true, options);
+            Browser2.JavascriptObjectRepository.Register("VRCX", VRCX.Instance, true, options);
+            Browser2.JavascriptObjectRepository.Register("VRCXStorage", VRCXStorage.Instance, false, options);
+            Browser2.JavascriptObjectRepository.Register("SQLite", SQLite.Instance, true, options);
             Browser1.IsBrowserInitializedChanged += (A, B) =>
             {
                 // Browser1.ShowDevTools();
