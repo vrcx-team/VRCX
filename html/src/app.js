7329b157e883404ed01ce66a8482a22859e4990c
Natsumi <cmcooper123@hotmail.com>
2021-02-26 02:06:48 +1300

    Fix moderation bugs

--- html/src/app.js
+++ html/src/app.js
@@ -2235,16 +2235,18 @@ speechSynthesis.getVoices();
     });
 
     API.$on('PLAYER-MODERATION:SEND', function (args) {
-        this.$emit('PLAYER-MODERATION', {
+        var ref = {
             json: args.json,
             params: {
                 playerModerationId: args.json.id
             }
-        });
+        };
+        this.$emit('PLAYER-MODERATION', ref);
+        this.$emit('PLAYER-MODERATION:@SEND', ref);
     });
 
     API.$on('PLAYER-MODERATION:DELETE', function (args) {
-        var { type, moderated } = args.param;
+        var { type, moderated } = args.params;
         var userId = this.currentUser.id;
         for (var ref of this.cachedPlayerModerations.values()) {
             if (ref.$isDeleted === false &&
@@ -7194,7 +7196,7 @@ speechSynthesis.getVoices();
         D.isFriend = false;
     });
 
-    API.$on('PLAYER-MODERATION', function (args) {
+    API.$on('PLAYER-MODERATION:@SEND', function (args) {
         var { ref } = args;
         var D = $app.userDialog;
         if (D.visible === false ||
@@ -7210,6 +7212,10 @@ speechSynthesis.getVoices();
         } else if (ref.type === 'hideAvatar') {
             D.isHideAvatar = true;
         }
+        $app.$message({
+            message: 'User moderated',
+            type: 'success'
+        });
     });
 
     API.$on('PLAYER-MODERATION:@DELETE', function (args) {
