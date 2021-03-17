23491805965c7217629c2166e48f4306f159e926
Natsumi <cmcooper123@hotmail.com>
2021-03-17 14:18:37 +1300

    Remove quick search limit + fix VRC Icon stretch
    useful when using memos for tagging

--- html/src/app.js
+++ html/src/app.js
@@ -5196,9 +5196,6 @@ speechSynthesis.getVoices();
                 }
                 return 0;
             });
-            if (results.length > 4) {
-                results.length = 4;
-            }
             results.push({
                 value: `search:${query}`,
                 label: query
