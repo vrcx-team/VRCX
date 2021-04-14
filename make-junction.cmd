c6453057656d3e7a7bb1eb8d601db35223102788
Natsumi <cmcooper123@hotmail.com>
2021-04-15 00:17:46 +1200

    Revert "Build dir moved"
    This reverts commit b716f5c6767d0b5f0931a5ba33b71e202541c610.

--- make-junction.cmd
+++ make-junction.cmd
@@ -1,3 +1,3 @@
-mklink /J "%~dp0\bin\Debug\html" "%~dp0\html\dist"
-mklink /J "%~dp0\bin\Release\html" "%~dp0\html\dist"
+mklink /J "%~dp0\bin\x64\Debug\html" "%~dp0\html\dist"
+mklink /J "%~dp0\bin\x64\Release\html" "%~dp0\html\dist"
 pause
