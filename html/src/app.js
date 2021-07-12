395777b2713aef43bd74bba191d13fe21ee802f9
Natsumi <cmcooper123@hotmail.com>
2021-07-13 01:54:07 +1200

    Add currentAvatarAssetUrl, typos

--- html/src/app.js
+++ html/src/app.js
@@ -9461,6 +9461,9 @@ speechSynthesis.getVoices();
         API.getAvatar({avatarId}).then((args) => {
             var { ref } = args;
             D.ref = ref;
+            if ((ref.imageUrl === API.currentUser.currentAvatarImageUrl) && (!ref.assetUrl)) {
+                D.ref.assetUrl = API.currentUser.currentAvatarAssetUrl;
+            }
             D.visible = true;
             if (/quest/.test(ref.tags)) {
                 D.isQuestFallback = true;
@@ -9480,7 +9483,7 @@ speechSynthesis.getVoices();
                 var fileId = extractFileId(ref.assetUrl);
                 var fileVersion = extractFileVersion(ref.assetUrl);
             }
-            var imageId = extractFileId(ref.thumbnailImageUrl);
+            var imageId = extractFileId(ref.imageUrl);
             D.fileSize = '';
             if (fileId) {
                 D.fileSize = 'Loading';
@@ -9620,7 +9623,7 @@ speechSynthesis.getVoices();
             });
             return;
         }
-        if ((refUserId === API.currentUser.id) && (API.cachedAvatars.has(API.currentUser.currentAvatar))) {
+        if (refUserId === API.currentUser.id) {
             this.showAvatarDialog(API.currentUser.currentAvatar);
             return;
         }
