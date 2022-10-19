a5401553eec08fd94c5622571f96c22e2aee1ca3
Natsumi <cmcooper123@hotmail.com>
2022-10-20 00:30:13 +1300

    direct access legacy user id's
    https://github.com/pypy-vrc/VRCX/issues/425

--- html/src/app.js
+++ html/src/app.js
@@ -13187,7 +13187,10 @@ speechSynthesis.getVoices();
                 this.showAvatarDialog(avatarId);
                 return true;
             }
-        } else if (input.substring(0, 4) === 'usr_') {
+        } else if (
+            input.substring(0, 4) === 'usr_' ||
+            /^[A-Za-z0-9]{10}$/g.test(input)
+        ) {
             this.showUserDialog(input.trim());
             return true;
         } else if (input.substring(0, 5) === 'avtr_') {
