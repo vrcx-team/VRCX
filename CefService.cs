8e5175b4f21c97e1bab5c61ec57894b5b5025b0a
Natsumi <cmcooper123@hotmail.com>
2022-11-05 00:57:30 +1300

    chore(.NET): update deps

--- CefService.cs
+++ CefService.cs
@@ -38,7 +38,6 @@ namespace VRCX
             cefSettings.CefCommandLineArgs.Add("disable-plugins");
             cefSettings.CefCommandLineArgs.Add("disable-spell-checking");
             cefSettings.CefCommandLineArgs.Add("disable-pdf-extension");
-            cefSettings.CefCommandLineArgs.Add("disable-extensions");
             cefSettings.CefCommandLineArgs["autoplay-policy"] = "no-user-gesture-required";
             // cefSettings.CefCommandLineArgs.Add("allow-universal-access-from-files");
             cefSettings.CefCommandLineArgs.Add("disable-web-security");
