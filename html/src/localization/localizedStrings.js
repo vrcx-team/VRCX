6ad9ad04feebd1afac7cf1e36100871aa305afad
Natsumi <cmcooper123@hotmail.com>
2022-12-26 10:53:51 +1300

    Add linter parser for import assert json

--- html/src/localization/localizedStrings.js
+++ html/src/localization/localizedStrings.js
@@ -1,5 +1,5 @@
-import en from './strings/en.json' assert { type: 'JSON' };
+import en from './strings/en.json' assert {type: 'JSON'};
 // import ja from './strings/ja.json' assert { type: 'JSON' };
-import zh_TW from './strings/zh_TW.json' assert { type: 'JSON' };
+import zh_TW from './strings/zh_TW.json' assert {type: 'JSON'};
 
-export { en, zh_TW };
+export {en, zh_TW};
