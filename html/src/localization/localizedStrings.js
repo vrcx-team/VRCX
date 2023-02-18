22548824b4bfc7ac8b4d74235856826139351b2d
flower_elf <47104993+flower-elf@users.noreply.github.com>
2023-02-18 14:47:48 +0800

    prettier?

--- html/src/localization/localizedStrings.js
+++ html/src/localization/localizedStrings.js
@@ -15,4 +15,9 @@ const localized_zh_TW = {...zh_TW, ...elements_zh_TW};
 const localized_zh_CN = {...zh_CN, ...elements_zh_CN};
 const localized_ko = {...ko, ...elements_ko};
 
-export {localized_en as en, localized_zh_TW as zh_TW, localized_ko as ko, localized_zh_CN as zh_CN};
+export {
+    localized_en as en,
+    localized_zh_TW as zh_TW,
+    localized_ko as ko,
+    localized_zh_CN as zh_CN
+};
