65f4fe9bf75274c37e4b18b91ac384a3d19f06a1
EbonCorvin <{ID}+{username}@users.noreply.github.com>
2024-06-25 11:42:30 -0400

    Allow user to review bulk unfriend list before processing

--- html/src/app.js
+++ html/src/app.js
@@ -22389,12 +22389,11 @@ speechSynthesis.getVoices();
     $app.data.friendsListBulkUnfriendMode = false;
 
     $app.methods.showBulkUnfriendSelectionConfirm = function () {
-        var elementsTicked = 0;
-        for (var ctx of this.friendsListTable.data) {
-            if (ctx.$selected) {
-                elementsTicked++;
-            }
-        }
+        var pendingUnfriendList = this.friendsListTable.data.reduce(
+            (acc, ctx, i) => (ctx.$selected && acc.push(ctx.displayName), acc),
+            []
+        );
+        var elementsTicked = pendingUnfriendList.length;
         if (elementsTicked === 0) {
             return;
         }
@@ -22407,6 +22406,9 @@ speechSynthesis.getVoices();
                 confirmButtonText: 'Confirm',
                 cancelButtonText: 'Cancel',
                 type: 'info',
+                showInput: true,
+                inputType: 'textarea',
+                inputValue: pendingUnfriendList.join('\r\n'),
                 callback: (action) => {
                     if (action === 'confirm') {
                         this.bulkUnfriendSelection();
