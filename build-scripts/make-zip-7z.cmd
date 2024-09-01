8c4e4af0752d83d7574f4f6c466c10ed1e0f405b
Usman Shafiq <ushafiq141@gmail.com>
2024-09-01 04:38:54 -0400

    Fix build scripts (#891)

--- build-scripts/make-zip-7z.cmd
+++ build-scripts/make-zip-7z.cmd
@@ -11,8 +11,8 @@ for /f %%a in ('powershell -Command "Get-Date -format yyyyMMdd"') do set TODAY=%
 set ZIP_NAME=VRCX_%TODAY%.zip
 echo %ZIP_NAME%
 rem using 7-Zip (https://www.7-zip.org)
-cd "%~dp0\bin\x64\Release"
+cd "%~dp0\..\bin\x64\Release"
 %ZIP_BIN% a -tzip %ZIP_NAME% * -mx=7 -xr0!*.log -xr0!*.pdb
 cd "%~dp0"
-move "%~dp0\bin\x64\Release\%ZIP_NAME%" "%~dp0"
+move "%~dp0\..\bin\x64\Release\%ZIP_NAME%" "%~dp0"
 pause
