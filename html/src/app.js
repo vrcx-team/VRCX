vrcx-team/VRCX

commit 992807a4610d3df0b5b7a727bf572cd5866a4e7c

UI: Enhancement (#1033) (#1034)
* ui: Enhancement (#1033)

* fix(style): bg color issue when hovering over input count in other themes

* fix(style): el-select height of feed tab

improve(style): status change style of feed page

improve(style): feed page

* improve(ui): favorites page

* improve(ui): tuning

* improve(style): settings page

html/src/app.js
@@ -7939,6 +7939,10 @@ speechSynthesis.getVoices();
         'VRCX_notificationTTSNickName',
         false
     );
+
+    // It's not necessary to store it in configRepo because it's rarely used.
+    $app.data.isTestTTSVisible = false;
+    
     $app.data.notificationTTSVoice = await configRepository.getString(
         'VRCX_notificationTTSVoice',
         '0'
@@ -17219,6 +17223,18 @@ speechSynthesis.getVoices();
         return style;
     };
 
+    $app.methods.userFavoriteWorldsStatusForFavTab = function (visibility) {
+        let style = '';
+        if (visibility === 'public') {
+            style = '';
+        } else if (visibility === 'friends') {
+            style = 'success';
+        } else {
+            style = 'info';
+        }
+        return style;
+    };
+
     $app.methods.changeWorldGroupVisibility = function (name, visibility) {
         var params = {
             type: 'world',
@@ -19690,6 +19706,23 @@ speechSynthesis.getVoices();
         this.worldExportDialogVisible = true;
     };
 
+    $app.methods.handleCopyWorldExportData = function (event) {
+        event.target.tagName === 'TEXTAREA' && event.target.select();
+        navigator.clipboard
+            .writeText(this.worldExportContent)
+            .then(() => {
+                this.$message({
+                    message: 'Copied successfully!',
+                    type: 'success',
+                    duration: 2000
+                });
+            })
+            .catch((err) => {
+                console.error('Copy failed:', err);
+                this.$message.error('Copy failed!');
+            });
+    };
+
     $app.methods.updateWorldExportDialog = function () {
         const formatter = function (str) {
             if (/[\x00-\x1f,"]/.test(str) === true) {
@@ -19955,6 +19988,23 @@ speechSynthesis.getVoices();
         this.avatarExportDialogVisible = true;
     };
 
+    $app.methods.handleCopyAvatarExportData = function (event) {
+        event.target.tagName === 'TEXTAREA' && event.target.select();
+        navigator.clipboard
+            .writeText(this.avatarExportContent)
+            .then(() => {
+                this.$message({
+                    message: 'Copied successfully!',
+                    type: 'success',
+                    duration: 2000
+                });
+            })
+            .catch((err) => {
+                console.error('Copy failed:', err);
+                this.$message.error('Copy failed!');
+            });
+    };
+
     /**
      * Update the content of the avatar export dialog based on the selected options
      */
@@ -20194,6 +20244,23 @@ speechSynthesis.getVoices();
         this.friendExportFavoriteGroup = null;
         this.updateFriendExportDialog();
         this.friendExportDialogVisible = true;
+    };
+
+    $app.methods.handleCopyFriendExportData = function (event) {
+        event.target.tagName === 'TEXTAREA' && event.target.select();
+        navigator.clipboard
+            .writeText(this.friendExportContent)
+            .then(() => {
+                this.$message({
+                    message: 'Copied successfully!',
+                    type: 'success',
+                    duration: 2000
+                });
+            })
+            .catch((err) => {
+                console.error('Copy failed:', err);
+                this.$message.error('Copy failed!');
+            });
     };
 
     $app.methods.updateFriendExportDialog = function () {

html/src/app.scss
@@ -407,7 +407,6 @@ img.friends-list-avatar {
     object-fit: cover;
 }
 
-.x-friend-item > .avatar.offline > img,
 .x-friend-item > .avatar.active > img {
     filter: grayscale(1);
 }
@@ -731,7 +730,21 @@ i.x-status-icon.red {
 
 .options-container-item .name {
     display: inline-block;
-    min-width: 175px;
+    min-width: 225px;
+}
+
+.simple-switch {
+    font-size: 12px;
+    margin-top: 5px;
+    display: flex;
+}
+
+.simple-switch .name {
+    flex-basis: 225px;
+}
+
+.simple-switch .switch {
+    flex-shrink: 0;
 }
 
 .toggle-switch {
@@ -749,7 +762,7 @@ i.x-status-icon.red {
 .toggle-list .toggle-name {
     display: inline-block;
     min-width: 190px;
-    padding-right: 5px;
+    padding-right: 10px;
     text-align: right;
 }
 
@@ -913,4 +926,28 @@ i.x-status-icon.red {
 
 .el-pagination .el-input .el-input__icon {
     line-height: 22px;
+}
+
+// User dialog memo: tag line-height
+.el-dialog__body .el-tag--mini {
+    line-height: 17px;
+}
+
+// feed table detail time tag line-height
+.el-table__expanded-cell .el-tag--mini {
+    line-height: 17px;
+}
+
+// User dialog memo: input count background color
+.x-friend-item:hover .el-input__count {
+    background: #f0f0f0;
+}
+
+// Align the left page with the right friend bar
+.x-app > .x-container {
+    padding-top: 15px;
+}
+
+.el-collapse-item .el-tag--mini {
+    line-height: 17px;
 }

html/src/mixins/tabs/profile.pug
@@ -1,5 +1,5 @@
 mixin profileTab()
-    .x-container(v-show="$refs.menu && $refs.menu.activeIndex === 'profile'" v-if="$refs.menu && $refs.menu.activeIndex === 'profile'")
+    .x-container(v-show="$refs.menu && $refs.menu.activeIndex === 'profile'")
         div.options-container(style="margin-top:0")
             span.header {{ $t('view.profile.profile.header') }}
             .x-friend-list(style="margin-top:10px")
@@ -21,8 +21,8 @@ mixin profileTab()
                     .detail
                         span.name {{ $t('view.profile.profile.v_bucks') }}
                         span.extra {{ API.currentUser.$vbucks ?? $t('view.profile.profile.refresh') }}
-            div
-                el-button(size="small" icon="el-icon-switch-button" @click="logout()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.logout') }}
+            div(style="margin-top:10px")
+                el-button(size="small" type="danger" plain icon="el-icon-switch-button" @click="logout()" style="margin-left:0;margin-right:5px;margin-top:10px;background:#fef0f0 !important;border-color:#fbc4c4 !important;") {{ $t('view.profile.profile.logout') }}
                 el-button(size="small" icon="el-icon-picture-outline" @click="showGalleryDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.manage_gallery_icon') }}
                 el-button(size="small" icon="el-icon-printer" @click="showExportFriendsListDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.export_friend_list') }}
                 el-button(size="small" icon="el-icon-user" @click="showExportAvatarsListDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.export_own_avatars') }}

html/src/theme.dark.scss
@@ -44,8 +44,13 @@ button {
 .el-textarea .el-input__count,
 .el-input .el-input__count .el-input__count-inner {
     color: #fff;
-    background-color: #444;
+    background-color: #333;
     border: #333;
+}
+
+// User dialog memo: input count background color
+.x-friend-item:hover .el-input__count {
+    background: #3e3e3e;
 }
 
 .el-input-group__append,
@@ -122,8 +127,9 @@ button {
     background-color: #333;
 }
 
-.el-tag--plain.el-tag--success {
+.el-collapse-item .el-tag--mini {
     background-color: #333;
+    border: transparent;
 }
 
 .el-button {

html/src/theme.material3.scss
@@ -1125,6 +1125,15 @@ input[type='number'],
     border-radius: 25px;
 }
 
+.x-friend-item .el-input__count {
+    background-color: transparent;
+}
+
+// User dialog memo: input count background color
+.x-friend-item:hover .el-input__count {
+    background-color: transparent;
+}
+
 .x-friend-item .detail {
     margin: 0 -2px;
 }
@@ -1987,4 +1996,9 @@ i.x-user-status {
 .el-table_41_column_287,
 .el-table_41_column_291 {
     text-align: center !important;
+}
+
+.el-collapse-item .el-tag--mini {
+    background-color: transparent;
+    border: transparent;
 }

html/src/mixins/dialogs/settings.pug
@@ -12,17 +12,17 @@ mixin settings()
         span GB
         el-tooltip(placement="top" :content="$t('dialog.config_json.refresh')" :disabled="hideTooltips")
             el-button(type="default" :loading="VRChatCacheSizeLoading" @click="getVRChatCacheSize" size="small" icon="el-icon-refresh" circle style="margin-left:5px")
-        br
-        span {{ $t('dialog.config_json.delete_all_cache') }}
-        el-button(size="small" style="margin-left:5px" icon="el-icon-delete" @click="showDeleteAllVRChatCacheConfirm()") {{ $t('dialog.config_json.delete_cache') }}
-        br
-        span {{ $t('dialog.config_json.delete_old_cache') }}
-        el-button(size="small" style="margin-left:5px" icon="el-icon-folder-delete" @click="sweepVRChatCache()") {{ $t('dialog.config_json.sweep_cache') }}
-        br
+        div(style="margin-top:10px")
+            span(style="margin-right:5px") {{ $t('dialog.config_json.delete_all_cache') }}
+            el-button(size="small" style="margin-left:5px" icon="el-icon-delete" @click="showDeleteAllVRChatCacheConfirm()") {{ $t('dialog.config_json.delete_cache') }}
+        div(style="margin-top:10px")
+            span(style="margin-right:5px") {{ $t('dialog.config_json.delete_old_cache') }}
+            el-button(size="small" style="margin-left:5px" icon="el-icon-folder-delete" @click="sweepVRChatCache()") {{ $t('dialog.config_json.sweep_cache') }}
         div(style="display:inline-block;margin-top:10px" v-for="(item, value) in VRChatConfigList" :key="value")
             span(v-text="item.name" style="word-break:keep-all")
             |:
-            el-input(v-model="VRChatConfigFile[value]" :placeholder="item.default" size="mini" :type="item.type?item.type:'text'" :min="item.min" :max="item.max")
+            el-input(v-model="VRChatConfigFile[value]" :placeholder="item.default" size="mini" :type="item.type?item.type:'text'" :min="item.min" :max="item.max" style="margin-top:5px")
+        br
         div(style="display:inline-block;margin-top:10px")
             span {{ $t('dialog.config_json.camera_resolution') }}
             br
@@ -52,9 +52,12 @@ mixin settings()
         el-checkbox(v-model="VRChatConfigFile.picture_output_split_by_date" style="margin-top:5px;display:block" :checked="true") {{ $t('dialog.config_json.picture_sort_by_date') }}
         el-checkbox(v-model="VRChatConfigFile.disableRichPresence" style="margin-top:5px;display:block") {{ $t('dialog.config_json.disable_discord_presence') }}
         template(#footer)
-            el-button(size="small" @click="openExternalLink('https://docs.vrchat.com/docs/configuration-file')") {{ $t('dialog.config_json.vrchat_docs') }}
-            el-button(size="small" @click="VRChatConfigDialog.visible = false") {{ $t('dialog.config_json.cancel') }}
-            el-button(type="primary" size="small" :disabled="VRChatConfigDialog.loading" @click="saveVRChatConfigFile") {{ $t('dialog.config_json.save') }}
+            div(style="display:flex;align-items:center;justify-content:space-between")
+                div
+                    el-button(size="small" @click="openExternalLink('https://docs.vrchat.com/docs/configuration-file')") {{ $t('dialog.config_json.vrchat_docs') }}
+                div
+                    el-button(size="small" @click="VRChatConfigDialog.visible = false") {{ $t('dialog.config_json.cancel') }}
+                    el-button(size="small" type="primary" :disabled="VRChatConfigDialog.loading" @click="saveVRChatConfigFile") {{ $t('dialog.config_json.save') }}
 
     //- dialog: YouTube Api Dialog
     el-dialog.x-dialog(:before-close="beforeDialogClose" @mousedown.native="dialogMouseDown" @mouseup.native="dialogMouseUp" ref="youTubeApiDialog" :visible.sync="youTubeApiDialog.visible" :title="$t('dialog.youtube_api.header')" width="400px")
@@ -150,13 +153,10 @@ mixin settings()
     //- dialog: Registry Auto Backup
     el-dialog.x-dialog(:before-close="beforeDialogClose" @closed="clearVrcRegistryDialog" @mousedown.native="dialogMouseDown" @mouseup.native="dialogMouseUp" ref="registryBackupDialog" :visible.sync="registryBackupDialog.visible" :title="$t('dialog.registry_backup.header')" width="600px")
         div(v-if="registryBackupDialog.visible" style="margin-top:10px")
-            div.options-container
-                div.options-container-item
-                    span.name {{ $t('dialog.registry_backup.auto_backup') }}
+            div.options-container(style="padding:0")
+                div.options-container-item(style="display:flex;align-items:center;justify-content:space-between")
+                    span.name(style="margin-right:24px") {{ $t('dialog.registry_backup.auto_backup') }}
                     el-switch(v-model="vrcRegistryAutoBackup" @change="saveVrcRegistryAutoBackup")
-            el-button(@click="promptVrcRegistryBackupName" size="small") {{ $t('dialog.registry_backup.backup') }}
-            el-button(@click="AppApi.OpenVrcRegJsonFileDialog()" size="small") {{ $t('dialog.registry_backup.restore_from_file') }}
-            el-button(@click="deleteVrcRegistry" size="small") {{ $t('dialog.registry_backup.reset') }}
             data-tables(v-bind="registryBackupTable" style="margin-top:10px")
                 el-table-column(:label="$t('dialog.registry_backup.name')" prop="name")
                 el-table-column(:label="$t('dialog.registry_backup.date')" prop="date")
@@ -170,6 +170,12 @@ mixin settings()
                             el-button(type="text" icon="el-icon-download" size="mini" @click="saveVrcRegistryBackupToFile(scope.row)")
                         el-tooltip(placement="top" :content="$t('dialog.registry_backup.delete')" :disabled="hideTooltips")
                             el-button(type="text" icon="el-icon-delete" size="mini" @click="deleteVrcRegistryBackup(scope.row)")
+            div(style="display:flex;align-items:center;justify-content:space-between;margin-top:10px")
+                el-button(type="danger" @click="deleteVrcRegistry" size="small") {{ $t('dialog.registry_backup.reset') }}
+                div
+                    el-button(@click="promptVrcRegistryBackupName" size="small") {{ $t('dialog.registry_backup.backup') }}
+                    el-button(@click="AppApi.OpenVrcRegJsonFileDialog()" size="small") {{ $t('dialog.registry_backup.restore_from_file') }}
+                
 
     //- dialog: Enable primary password
     el-dialog.x-dialog(

html/src/classes/uiComponents.js
@@ -73,7 +73,7 @@ export default class extends baseClass {
                 '<i v-if="isTraveling" class="el-icon el-icon-loading" style="display:inline-block;margin-right:5px"></i>' +
                 '<span>{{ text }}</span></span>' +
                 '<span v-if="groupName" @click="showGroupDialog" :class="{ \'x-link\': link}">({{ groupName }})</span>' +
-                '<span class="flags" :class="region" style="display:inline-block;margin-left:5px"></span>' +
+                '<span v-if="region" class="flags" :class="region" style="display:inline-block;margin-left:5px"></span>' +
                 '<i v-if="strict" class="el-icon el-icon-lock" style="display:inline-block;margin-left:5px"></i></span>',
             props: {
                 location: String,

commit 1156b77b6c75e9c3a3ffaee1b2013e4857304d65

fix: issues caused by PR(#1034). (#1033)

html/src/app.js
@@ -133,10 +133,16 @@ speechSynthesis.getVoices();
             isRunningUnderWine: false,
             appVersion: '',
             latestAppVersion: '',
-            shiftHeld: false
+            shiftHeld: false,
+            showTooltips: false
         },
         i18n,
-        computed: {},
+        computed: {
+            // REVIEW: Temp hide tooltips and fix boolean-switch mismatch.
+            hideTooltips: function () {
+                return !this.showTooltips;
+            }
+        },
         methods: {
             ...$utils
         },
@@ -7927,8 +7933,8 @@ speechSynthesis.getVoices();
         'displayVRCPlusIconsAsAvatar',
         true
     );
-    $app.data.hideTooltips = await configRepository.getBool(
-        'VRCX_hideTooltips',
+    $app.data.showTooltips = await configRepository.getBool(
+        'VRCX_showTooltips',
         false
     );
     $app.data.hideNicknames = await configRepository.getBool(
@@ -8145,7 +8151,7 @@ speechSynthesis.getVoices();
             'displayVRCPlusIconsAsAvatar',
             this.displayVRCPlusIconsAsAvatar
         );
-        await configRepository.setBool('VRCX_hideTooltips', this.hideTooltips);
+        await configRepository.setBool('VRCX_showTooltips', this.showTooltips);
         await configRepository.setBool(
             'VRCX_hideNicknames',
             this.hideNicknames

html/src/app.scss
@@ -784,7 +784,7 @@ i.x-status-icon.red {
     border-color: #67c23a !important;
 }
 
-.el-button--danger {
+.x-dialog .el-button--danger {
     background-color: #f56c6c !important;
     border-color: #f56c6c !important;
 }

html/src/mixins/tabs/profile.pug
@@ -22,7 +22,7 @@ mixin profileTab()
                         span.name {{ $t('view.profile.profile.v_bucks') }}
                         span.extra {{ API.currentUser.$vbucks ?? $t('view.profile.profile.refresh') }}
             div(style="margin-top:10px")
-                el-button(size="small" type="danger" plain icon="el-icon-switch-button" @click="logout()" style="margin-left:0;margin-right:5px;margin-top:10px;background:#fef0f0 !important;border-color:#fbc4c4 !important;") {{ $t('view.profile.profile.logout') }}
+                el-button(size="small" type="danger" plain icon="el-icon-switch-button" @click="logout()" style="margin-left:0;margin-right:5px;margin-top:10px;") {{ $t('view.profile.profile.logout') }}
                 el-button(size="small" icon="el-icon-picture-outline" @click="showGalleryDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.manage_gallery_icon') }}
                 el-button(size="small" icon="el-icon-printer" @click="showExportFriendsListDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.export_friend_list') }}
                 el-button(size="small" icon="el-icon-user" @click="showExportAvatarsListDialog()" style="margin-left:0;margin-right:5px;margin-top:10px") {{ $t('view.profile.profile.export_own_avatars') }}

html/src/mixins/tabs/settings.pug
@@ -201,7 +201,7 @@ mixin settingsTab()
                         el-input-number(size="small" v-model="zoomLevel" @change="setZoomLevel" :precision="0" style="width:128px")
                     +simpleSwitch('view.settings.appearance.appearance.vrcplus_profile_icons', "displayVRCPlusIconsAsAvatar", "saveOpenVROption")
                     +simpleSwitch('view.settings.appearance.appearance.nicknames', "hideNicknames", "saveOpenVROption")
-                    +simpleSwitch('view.settings.appearance.appearance.tooltips', "hideTooltips", "saveOpenVROption")                    
+                    +simpleSwitch('view.settings.appearance.appearance.tooltips', "showTooltips", "saveOpenVROption")
                     div.options-container-item
                         span.name {{ $t('view.settings.appearance.appearance.sort_favorite_by') }}
                         el-radio-group(v-model="sortFavorites" @change="saveSortFavoritesOption")

html/src/theme.dark.scss
@@ -44,8 +44,12 @@ button {
 .el-textarea .el-input__count,
 .el-input .el-input__count .el-input__count-inner {
     color: #fff;
-    background-color: #333;
+    background-color: #444;
     border: #333;
+}
+
+.x-friend-item .el-textarea .el-input__count {
+    background-color: #333;
 }
 
 // User dialog memo: input count background color

html/src/theme.material3.scss
@@ -212,7 +212,7 @@ div[style*='margin: 0px 0px 10px;'] {
     flex: 1;
 }
 
-.options-container-item *:not(.el-color-picker__color-inner):last-child {
+.options-container-item > :not(.el-color-picker__color-inner):last-child {
     margin-right: 4px;
 }
 
@@ -988,7 +988,7 @@ input[type='number'],
 }
 .el-button:not(.el-button--text, .el-button--primary)
     > i:first-child:last-child {
-    padding: 0 11px;
+    padding: 5px 10px;
 }
 .el-button:not(.el-button--text, .el-button--primary) > i:first-child {
     padding-left: 16px;
@@ -2001,4 +2001,16 @@ i.x-user-status {
 .el-collapse-item .el-tag--mini {
     background-color: transparent;
     border: transparent;
+}
+
+.simple-switch {
+    font-size: 12px;
+    margin-top: 5px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.el-dialog__body .el-tag--mini {
+    line-height: 28px;
 }