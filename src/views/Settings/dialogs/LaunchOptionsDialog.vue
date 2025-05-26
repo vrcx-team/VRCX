58b4caadb92d1a80c5ab5f2646c327fd494e8abc
Natsumi <cmcooper123@hotmail.com>
2025-05-26 18:52:36 +1000

    clean up isLinux()

--- src/views/Settings/dialogs/LaunchOptionsDialog.vue
+++ src/views/Settings/dialogs/LaunchOptionsDialog.vue
@@ -28,7 +28,7 @@
         </div>
 
         <el-input
-            v-if="!isLinux()"
+            v-if="!islinux"
             v-model="launchOptionsDialog.vrcLaunchPathOverride"
             type="textarea"
             placeholder="C:\Program Files (x86)\Steam\steamapps\common\VRChat"
