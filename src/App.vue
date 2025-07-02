491606bb493249510a9aed2b36200a71c4532a9a
pa <maplenagisa@gmail.com>
2025-07-02 21:33:41 +0900

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <script>
     import Vue from 'vue';
     import template from './app.pug';
-
-    import AvatarDialog from './components/dialogs/AvatarDialog/AvatarDialog.vue';
-    import ChooseFavoriteGroupDialog from './components/dialogs/ChooseFavoriteGroupDialog.vue';
-    import FullscreenImageDialog from './components/dialogs/FullscreenImageDialog.vue';
-    import GalleryDialog from './components/dialogs/GalleryDialog.vue';
-    import GroupDialog from './components/dialogs/GroupDialog/GroupDialog.vue';
-    import LaunchDialog from './components/dialogs/LaunchDialog.vue';
-    import PreviousInstancesInfoDialog from './components/dialogs/PreviousInstancesDialog/PreviousInstancesInfoDialog.vue';
-    import UserDialog from './components/dialogs/UserDialog/UserDialog.vue';
-    import VRCXUpdateDialog from './components/dialogs/VRCXUpdateDialog.vue';
-    import WorldDialog from './components/dialogs/WorldDialog/WorldDialog.vue';
-    import NavMenu from './components/NavMenu.vue';
-    import configRepository from './service/config.js';
-
-    import { refreshCustomCss, refreshCustomScript } from './shared/utils';
-    import { utils } from './shared/utils/_utils';
     import { createGlobalStores } from './stores';
-    import ChartsTab from './views/Charts/Charts.vue';
-    import AvatarImportDialog from './views/Favorites/dialogs/AvatarImportDialog.vue';
-    import FriendImportDialog from './views/Favorites/dialogs/FriendImportDialog.vue';
-    import WorldImportDialog from './views/Favorites/dialogs/WorldImportDialog.vue';
-    import FavoritesTab from './views/Favorites/Favorites.vue';
-    import FeedTab from './views/Feed/Feed.vue';
-    import FriendListTab from './views/FriendList/FriendList.vue';
-    import FriendLogTab from './views/FriendLog/FriendLog.vue';
-    import GameLogTab from './views/GameLog/GameLog.vue';
+
     import LoginPage from './views/Login/Login.vue';
+    import NavMenu from './components/NavMenu.vue';
+    import Sidebar from './views/Sidebar/Sidebar.vue';
+    import FeedTab from './views/Feed/Feed.vue';
+    import GameLogTab from './views/GameLog/GameLog.vue';
+    import PlayerListTab from './views/PlayerList/PlayerList.vue';
+    import SearchTab from './views/Search/Search.vue';
+    import FavoritesTab from './views/Favorites/Favorites.vue';
+    import FriendLogTab from './views/FriendLog/FriendLog.vue';
     import ModerationTab from './views/Moderation/Moderation.vue';
     import NotificationTab from './views/Notifications/Notification.vue';
-    import PlayerListTab from './views/PlayerList/PlayerList.vue';
-    import EditInviteMessageDialog from './views/Profile/dialogs/EditInviteMessageDialog.vue';
+    import FriendListTab from './views/FriendList/FriendList.vue';
+    import ChartsTab from './views/Charts/Charts.vue';
     import ProfileTab from './views/Profile/Profile.vue';
-    import SearchTab from './views/Search/Search.vue';
-    import LaunchOptionsDialog from './views/Settings/dialogs/LaunchOptionsDialog.vue';
-    import PrimaryPasswordDialog from './views/Settings/dialogs/PrimaryPasswordDialog.vue';
-    import VRChatConfigDialog from './views/Settings/dialogs/VRChatConfigDialog.vue';
     import SettingsTab from './views/Settings/Settings.vue';
-    import Sidebar from './views/Sidebar/Sidebar.vue';
 
+    import UserDialog from './components/dialogs/UserDialog/UserDialog.vue';
+    import WorldDialog from './components/dialogs/WorldDialog/WorldDialog.vue';
+    import AvatarDialog from './components/dialogs/AvatarDialog/AvatarDialog.vue';
+    import GroupDialog from './components/dialogs/GroupDialog/GroupDialog.vue';
+    import GalleryDialog from './components/dialogs/GalleryDialog.vue';
+    import FullscreenImageDialog from './components/dialogs/FullscreenImageDialog.vue';
+    import PreviousInstancesInfoDialog from './components/dialogs/PreviousInstancesDialog/PreviousInstancesInfoDialog.vue';
+    import LaunchDialog from './components/dialogs/LaunchDialog.vue';
+    import LaunchOptionsDialog from './views/Settings/dialogs/LaunchOptionsDialog.vue';
+    import FriendImportDialog from './views/Favorites/dialogs/FriendImportDialog.vue';
+    import WorldImportDialog from './views/Favorites/dialogs/WorldImportDialog.vue';
+    import AvatarImportDialog from './views/Favorites/dialogs/AvatarImportDialog.vue';
+    import ChooseFavoriteGroupDialog from './components/dialogs/ChooseFavoriteGroupDialog.vue';
+    import EditInviteMessageDialog from './views/Profile/dialogs/EditInviteMessageDialog.vue';
+    import VRCXUpdateDialog from './components/dialogs/VRCXUpdateDialog.vue';
+    import VRChatConfigDialog from './views/Settings/dialogs/VRChatConfigDialog.vue';
+    import PrimaryPasswordDialog from './views/Settings/dialogs/PrimaryPasswordDialog.vue';
+
+    import { utils } from './shared/utils/_utils';
+    import { refreshCustomCss, refreshCustomScript } from './shared/utils';
+    import configRepository from './service/config.js';
     await configRepository.init();
     const initThemeMode = await configRepository.getString('VRCX_ThemeMode', 'system');
 
@@ -67,23 +67,24 @@
             ProfileTab,
             SettingsTab,
             Sidebar,
-            PreviousInstancesInfoDialog,
+
             UserDialog,
             WorldDialog,
-            GroupDialog,
             AvatarDialog,
+            GroupDialog,
+            GalleryDialog,
+            FullscreenImageDialog,
+            PreviousInstancesInfoDialog,
+            LaunchDialog,
+            LaunchOptionsDialog,
             FriendImportDialog,
             WorldImportDialog,
             AvatarImportDialog,
             ChooseFavoriteGroupDialog,
-            LaunchDialog,
-            LaunchOptionsDialog,
-            VRCXUpdateDialog,
             EditInviteMessageDialog,
+            VRCXUpdateDialog,
             VRChatConfigDialog,
-            PrimaryPasswordDialog,
-            FullscreenImageDialog,
-            GalleryDialog
+            PrimaryPasswordDialog
         },
         methods: {
             /**
