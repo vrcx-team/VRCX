70d57f7499ea45c9409188a50ecdddde1fef35d1
Natsumi <cmcooper123@hotmail.com>
2025-09-15 14:28:04 +1200

    Small fixes, fix open instance game crash

--- src/components/InviteYourself.vue
+++ src/components/InviteYourself.vue
@@ -7,16 +7,22 @@
             <el-button v-show="isVisible" @click="confirmInvite" size="small" :icon="Message" circle />
         </el-tooltip>
         <el-tooltip v-else placement="top" :content="t('dialog.user.info.open_in_vrchat_tooltip')">
-            <el-button @click="openInstance" size="small" :icon="Message" circle />
+            <el-button v-if="isOpeningInstance" size="small" circle>
+                <el-icon class="is-loading">
+                    <Loading />
+                </el-icon>
+            </el-button>
+            <el-button v-else @click="openInstance" size="small" :icon="Message" circle />
         </el-tooltip>
     </div>
 </template>
 
 <script setup>
     import { ElMessage } from 'element-plus';
-    import { Message } from '@element-plus/icons-vue';
+    import { Loading, Message } from '@element-plus/icons-vue';
     import { computed } from 'vue';
     import { useI18n } from 'vue-i18n';
+    import { storeToRefs } from 'pinia';
     import { instanceRequest } from '../api';
     import { checkCanInviteSelf, parseLocation } from '../shared/utils';
     import { useInviteStore, useLaunchStore } from '../stores';
@@ -30,6 +36,8 @@
 
     const { canOpenInstanceInGame } = useInviteStore();
     const { tryOpenInstanceInVrc } = useLaunchStore();
+
+    const { isOpeningInstance } = storeToRefs(useLaunchStore());
 
     const isVisible = computed(() => checkCanInviteSelf(props.location));
 
