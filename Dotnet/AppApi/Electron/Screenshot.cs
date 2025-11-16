ee33b51fcd1ee24e5ff5a361974f1581c6bb52e0
Natsumi <cmcooper123@hotmail.com>
2025-11-16 17:11:18 +1100

    Update Cef to .NET 10

--- Dotnet/AppApi/Electron/Screenshot.cs
+++ Dotnet/AppApi/Electron/Screenshot.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
