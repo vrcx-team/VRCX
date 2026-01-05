de01638b1867e3c06a98c7ef7013f52edb9e6668
Natsumi <cmcooper123@hotmail.com>
2026-01-05 21:23:01 +1300

    Fix sidebar icons

--- src/vite.config.js
+++ src/vite.config.js
@@ -106,7 +106,7 @@ export default defineConfig(({ mode }) => {
             outDir: '../build/html',
             license: true,
             emptyOutDir: true,
-            copyPublicDir: false,
+            copyPublicDir: true,
             reportCompressedSize: false,
             chunkSizeWarningLimit: 5000,
             sourcemap: buildAndUploadSourceMaps,
