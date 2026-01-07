dd45ca9545d4582b78eec8853e18bd3752c55f5a
pa <maplenagisa@gmail.com>
2026-01-07 23:03:58 +0900

    revert tooltip component to initial state

--- src/components/ui/tooltip/Tooltip.vue
+++ src/components/ui/tooltip/Tooltip.vue
@@ -1,18 +1,18 @@
 <script setup>
-    import { TooltipRoot, useForwardPropsEmits } from 'reka-ui';
+import { TooltipRoot, useForwardPropsEmits } from 'reka-ui';
 
-    const props = defineProps({
-        defaultOpen: { type: Boolean, required: false },
-        open: { type: Boolean, required: false },
-        delayDuration: { type: Number, required: false },
-        disableHoverableContent: { type: Boolean, required: false },
-        disableClosingTrigger: { type: Boolean, required: false },
-        disabled: { type: Boolean, required: false },
-        ignoreNonKeyboardFocus: { type: Boolean, required: false }
-    });
-    const emits = defineEmits(['update:open']);
+const props = defineProps({
+    defaultOpen: { type: Boolean, required: false },
+    open: { type: Boolean, required: false },
+    delayDuration: { type: Number, required: false },
+    disableHoverableContent: { type: Boolean, required: false },
+    disableClosingTrigger: { type: Boolean, required: false },
+    disabled: { type: Boolean, required: false },
+    ignoreNonKeyboardFocus: { type: Boolean, required: false }
+});
+const emits = defineEmits(['update:open']);
 
-    const forwarded = useForwardPropsEmits(props, emits);
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
