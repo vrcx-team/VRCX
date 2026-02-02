1cbafbeaeb07ff873ce8a10fe3aa1a1d309d11b0
Natsumi <cmcooper123@hotmail.com>
2026-02-03 03:29:18 +1300

    Fix macOS titlebar

--- src/components/ui/sidebar/Sidebar.vue
+++ src/components/ui/sidebar/Sidebar.vue
@@ -86,7 +86,7 @@
         <div
             :class="
                 cn(
-                    'fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) md:flex',
+                    'absolute inset-y-0 z-10 hidden w-(--sidebar-width) md:flex',
                     side === 'left'
                         ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
                         : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
