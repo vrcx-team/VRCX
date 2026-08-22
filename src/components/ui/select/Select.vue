08b526074e40de1c545d097ab3ec56e00a009729
Natsumi <cmcooper123@hotmail.com>
2026-08-22 16:54:26 +1000

    Fix warning

--- src/components/ui/select/Select.vue
+++ src/components/ui/select/Select.vue
@@ -17,10 +17,21 @@
     const emits = defineEmits(['update:modelValue', 'update:open']);
 
     const forwarded = useForwardPropsEmits(props, emits);
+
+    function handleOpenChange(isOpen) {
+        if (!isOpen || typeof document === 'undefined') {
+            return;
+        }
+
+        const activeElement = document.activeElement;
+        if (activeElement instanceof HTMLElement && activeElement.dataset.slot === 'select-trigger') {
+            activeElement.blur();
+        }
+    }
 </script>
 
 <template>
-    <SelectRoot v-slot="slotProps" data-slot="select" v-bind="forwarded">
+    <SelectRoot v-slot="slotProps" data-slot="select" v-bind="forwarded" @update:open="handleOpenChange">
         <slot v-bind="slotProps" />
     </SelectRoot>
 </template>
