cdd796444e4408048ca9ddce55435b6ab3285501
NCIOlar <151127865+NCIOlar@users.noreply.github.com>
2026-08-30 05:46:06 -0400

    feat: add auto decline spam friend requests (#1867)

--- src/stores/notification/index.js
+++ src/stores/notification/index.js
@@ -283,6 +283,12 @@ export const useNotificationStore = defineStore('Notification', () => {
     function handlePipelineNotification(args) {
         const ref = args.json;
         if (
+            ref.type === 'friendRequest' &&
+            generalSettingsStore.autoDeclineFriendRequests
+        ) {
+            handleAutoDeclineFriendRequest(ref);
+        }
+        if (
             ref.type !== 'requestInvite' ||
             generalSettingsStore.autoAcceptInviteRequests === 'Off'
         ) {
@@ -392,6 +398,22 @@ export const useNotificationStore = defineStore('Notification', () => {
                         console.error(err);
                     });
             });
+    }
+
+    async function handleAutoDeclineFriendRequest(ref) {
+        const joinInfo = await database.getJoinCount({
+            id: ref.senderUserId,
+            displayName: ref.senderUsername
+        });
+
+        if (Number(joinInfo.joinCount) !== 0) {
+            return;
+        }
+
+        await notificationRequest.hideNotification({
+            notificationId: ref.id
+        });
+        handleNotificationHide(ref.id);
     }
 
     /**
