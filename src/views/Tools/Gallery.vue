104fa6b9a7669ca10ccaf83d1a15f75e6d300ae0
Natsumi <cmcooper123@hotmail.com>
2026-09-17 22:39:19 +1000

    Fix setting user icon for gallery

--- src/views/Tools/Gallery.vue
+++ src/views/Tools/Gallery.vue
@@ -69,14 +69,6 @@
                             <Upload />
                             {{ t('dialog.gallery_icons.upload') }}
                         </Button>
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            :disabled="!currentUser.profilePicOverride"
-                            @click="setProfilePicOverride('')">
-                            <X />
-                            {{ t('dialog.gallery_icons.clear') }}
-                        </Button>
                     </ButtonGroup>
                     <ItemGroup
                         class="grid gap-3 mt-3"
@@ -87,7 +79,6 @@
                             variant="outline"
                             size="sm"
                             class="p-0 x-hover-card hover:bg-accent hover:shadow-sm"
-                            :class="compareCurrentProfilePic(image.id) ? 'x-highlight-ring' : ''"
                             as-child>
                             <div
                                 v-if="
@@ -113,13 +104,6 @@
                                         class="rounded-full text-destructive"
                                         @click="deleteGalleryImage(image.id)">
                                         <Trash2 />
-                                    </Button>
-                                    <Button
-                                        size="icon-sm"
-                                        class="rounded-full"
-                                        :variant="compareCurrentProfilePic(image.id) ? 'default' : 'ghost'"
-                                        @click="setProfilePicOverride(image.id)">
-                                        <Check />
                                     </Button>
                                 </ItemFooter>
                             </div>
@@ -152,8 +136,8 @@
                         <Button
                             variant="outline"
                             size="sm"
-                            :disabled="!currentUser.userIcon"
-                            @click="setVRCPlusIcon('')">
+                            :disabled="currentUser.userIcon === currentUser.currentAvatarImageUrl"
+                            @click="setUserIcon('')">
                             <X />
                             {{ t('dialog.gallery_icons.clear') }}
                         </Button>
@@ -198,7 +182,7 @@
                                         size="icon-sm"
                                         class="rounded-full"
                                         :variant="compareCurrentVRCPlusIcon(image.id) ? 'default' : 'ghost'"
-                                        @click="setVRCPlusIcon(image.id)">
+                                        @click="setUserIcon(image.id)">
                                         <Check />
                                     </Button>
                                 </ItemFooter>
@@ -841,17 +825,11 @@
     /**
      * @param {Event} e
      * @param {{
-
      *     inputSelector: string;
-
      *     aspectRatio: number;
-
      *     beforeCrop?: (file: File) => void;
-
      *     upload: (payload: { file: File; blob: Blob; base64Body: string }) => Promise<void>;
-
      *     errorMessage?: string;
-
      * }} options
      */
     function openImageUploadFlow(
@@ -916,38 +894,6 @@
     /**
      * @param fileId
      */
-    function setProfilePicOverride(fileId) {
-        if (!isLocalUserVrcPlusSupporter.value) {
-            toast.error(t('message.vrcplus.required'));
-            return;
-        }
-        let profilePicOverride = '';
-        if (fileId) {
-            profilePicOverride = `${AppDebug.endpointDomain}/file/${fileId}/1`;
-        }
-        if (profilePicOverride === currentUser.value.profilePicOverride) {
-            return;
-        }
-        userRequest
-            .saveCurrentUser({
-                profilePicOverride
-            })
-            .then((args) => {
-                toast.success(t('message.gallery.profile_pic_changed'));
-                return args;
-            });
-    }
-
-    /**
-     * @param fileId
-     */
-    function compareCurrentProfilePic(fileId) {
-        return isCurrentFile(currentUser.value.profilePicOverride, fileId);
-    }
-
-    /**
-     * @param fileId
-     */
     function deleteGalleryImage(fileId) {
         deleteFileAndRemove(fileId, galleryTable.value);
     }
@@ -979,7 +925,7 @@
     /**
      * @param fileId
      */
-    function setVRCPlusIcon(fileId) {
+    function setUserIcon(fileId) {
         if (!isLocalUserVrcPlusSupporter.value) {
             toast.error(t('message.vrcplus.required'));
             return;
@@ -988,16 +934,15 @@
         if (fileId) {
             userIcon = `${AppDebug.endpointDomain}/file/${fileId}/1`;
         }
-        if (userIcon === currentUser.value.userIcon) {
+        if (userIcon === currentUser.value.iconUrl) {
             return;
         }
         userRequest
-            .saveCurrentUser({
+            .saveProfile({
                 userIcon
             })
-            .then((args) => {
+            .then(() => {
                 toast.success(t('message.gallery.profile_icon_changed'));
-                return args;
             });
     }
 
@@ -1005,7 +950,7 @@
      * @param userIcon
      */
     function compareCurrentVRCPlusIcon(userIcon) {
-        return isCurrentFile(currentUser.value.userIcon, userIcon);
+        return isCurrentFile(currentUser.value.iconUrl, userIcon);
     }
 
     /**
